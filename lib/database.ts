a3088b9596c5a778ce52ef756d2041a8d875973a
Hemit Patel <hemitvpatel@gmail.com>
2025-09-08 07:29:32 -0400

    use neon specialized dialects to connect to our postgres db

--- lib/database.ts
+++ lib/database.ts
@@ -1,23 +1,12 @@
-import { Database } from '@/types/database' // this is the Database interface we defined earlier
-import { Pool } from 'pg'
-import { Kysely, PostgresDialect } from 'kysely'
+import { Database } from '@/types/database' 
+import { Kysely } from 'kysely'
+import { NeonDialect } from 'kysely-neon';
+import { neon } from "@neondatabase/serverless";
 
-const dialect = new PostgresDialect({
-  pool: new Pool({
-    database: process.env.PGDATABASE,
-    host: process.env.PGHOST,
-    user: process.env.PGUSER,
-    password: process.env.PGPASSWORD,
-    port: 5434,
-    max: 10,
-    ssl: { rejectUnauthorized: false }, // for Neon
-  })
+const dialect = new NeonDialect({
+  neon: neon(process.env.CONN_STRING)
 })
 
-// Database interface is passed to Kysely's constructor, and from now on, Kysely 
-// knows your database structure.
-// Dialect is passed to Kysely's constructor, and from now on, Kysely knows how 
-// to communicate with your database.
 export const db = new Kysely<Database>({
-  dialect,
+  dialect
 })
