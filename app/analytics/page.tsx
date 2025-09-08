85d117bb4dc54ec081710c20d04c2fef0331c002
Hemit Patel <hemitvpatel@gmail.com>
2025-09-08 11:45:37 -0400

    fix loading flow

--- app/analytics/page.tsx
+++ app/analytics/page.tsx
@@ -5,7 +5,7 @@ import React, { useEffect, useState } from 'react'
 
 const Analytics = () => {
   const [analytics, setAnalytics] = useState<AnalyticsType | null>(null);
-  const [isLoading, setIsLoading] = useState(false);
+  const [isLoading, setIsLoading] = useState(true);
 
   useEffect(() => {
     const handleGetAnalytics = async () => {
@@ -25,17 +25,16 @@ const Analytics = () => {
   }, []);
 
   if (isLoading) return <p className="text-gray-500">Loading...</p>;
-  if (!analytics) return <p className="text-gray-500">No analytics data</p>;
 
   return (
     <div className="grid grid-cols-2 gap-4 max-w-md mx-auto mt-6">
       <div className="p-6 bg-white shadow rounded-2xl text-center">
         <h2 className="text-lg font-semibold text-gray-700">Requests</h2>
-        <p className="text-3xl font-bold text-blue-600">{analytics.requestCount}</p>
+        <p className="text-3xl font-bold text-blue-600">{analytics?.requestCount}</p>
       </div>
       <div className="p-6 bg-white shadow rounded-2xl text-center">
         <h2 className="text-lg font-semibold text-gray-700">Responses</h2>
-        <p className="text-3xl font-bold text-green-600">{analytics.responseCount}</p>
+        <p className="text-3xl font-bold text-green-600">{analytics?.responseCount}</p>
       </div>
     </div>
   );
