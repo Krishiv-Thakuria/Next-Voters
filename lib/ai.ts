b835bda6e7adbf508050bf97ce2a69421a378037
Hemit Patel <hemitvpatel@gmail.com>
2025-09-12 10:47:11 -0400

    add basic code

---
+++ lib/ai.ts
@@ -0,0 +1,18 @@
+import { generateObject } from 'ai';
+import { z } from 'zod';
+
+const generateLLMResponse = () => {
+    const { object } = await generateObject({
+    model: 'openai/gpt-4.1',
+    schema: z.object({
+        recipe: z.object({
+        name: z.string(),
+        ingredients: z.array(z.object({ name: z.string(), amount: z.string() })),
+        steps: z.array(z.string()),
+        }),
+    }),
+    prompt: 'Generate a lasagna recipe.',
+    });
+
+    return object.message
+}
