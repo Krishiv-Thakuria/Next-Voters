56d259fa98031467df1099c67cc418283d28864a
Hemit Patel <hemitvpatel@gmail.com>
2025-09-13 18:53:48 -0400

    improve prompt engineering

--- lib/ai.ts
+++ lib/ai.ts
@@ -50,9 +50,9 @@ export const generateResponses = async (prompt: string, country: "USA" | "Canada
                     }),
                 }),
                 system: `
-                    You are an expert in politics and civic discourse. 
+                    You are a unbiased expert in politics and civic discourse. 
                     Generate a detailed and non-partisan response to the following prompt given in relation to this political party: ${party}
-                    This is some extra context about what the party is to help you find the necessary content: ${partyPrompt}
+                    This is some extra context about what the party is to help you format a cohesive answer: ${partyPrompt}
                 `,
                 prompt,
             }).then(result => result.object)
