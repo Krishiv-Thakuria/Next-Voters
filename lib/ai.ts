fdbfb491644a3ebec502212d2ebe02e1b660f8f4
Hemit Patel <hemitvpatel@gmail.com>
2025-09-12 22:41:57 -0400

    update formatting

--- lib/ai.ts
+++ lib/ai.ts
@@ -4,15 +4,15 @@ import { z } from 'zod';
 
 export const generateResponse = async () => {
     const { object } = await generateObject({
-    model: groq('openai/gpt-4.1'),
-    schema: z.object({
-        recipe: z.object({
-        name: z.string(),
-        ingredients: z.array(z.object({ name: z.string(), amount: z.string() })),
-        steps: z.array(z.string()),
+        model: groq('openai/gpt-4.1'),
+        schema: z.object({
+            recipe: z.object({
+            name: z.string(),
+            ingredients: z.array(z.object({ name: z.string(), amount: z.string() })),
+            steps: z.array(z.string()),
+            }),
         }),
-    }),
-    prompt: 'Generate a lasagna recipe.',
+        prompt: 'Generate a lasagna recipe.',
     });
 
     return object
