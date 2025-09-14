a060649caa88a11796a9576edcc4ff8dd09c0e9b
Hemit Patel <hemitvpatel@gmail.com>
2025-09-13 22:17:15 -0400

    added some more stuff for cli

--- cli/run.ts
+++ cli/run.ts
@@ -4,14 +4,36 @@ import inquirer from "inquirer";
 
 log(chalk.blue("Welcome to Next Voters' CLI tooling!"));
 
-inquirer
-  .prompt([
-    {
-      type: "input",
-      name: "documentLink",
-      message: "What is the link of your document?"
+try {
+    const answers = await inquirer.prompt([
+        {
+            type: "input",
+            name: "documentLink",
+            message: "What is the link of your document?",
+            validate: (input) => {
+                const urlRegex = /^(https?:\/\/[^\s$.?#].[^\s]*)$/i;
+                return urlRegex.test(input) || "Please enter a valid URL starting with http:// or https://";
+            }
+        },
+    ]);
+
+    const documentLink = answers.documentLink;
+    log(chalk.green("Valid link inputted. Checking document type..."));
+
+    const response = await fetch(documentLink);
+
+    if (!response.ok) {
+        throw new Error(`Failed to fetch document. Status: ${response.status}`);
     }
-  ])
-  .then((answers) => {
-    log(answers.documentLink);
-  });
+
+    const contentType = response.headers.get("content-type") || "";
+
+    if (contentType.includes("application/pdf")) {
+        log(chalk.green("sigma"));
+    } else {
+        throw new Error("The link did not return a PDF document.");
+    }
+
+} catch (error) {
+    log(chalk.red("AN ERROR OCCURRED!! "), error.message);
+}
