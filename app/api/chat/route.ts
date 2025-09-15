066f37e5a9caf785d08ad6e1c3983d0c93c851ed
Hemit Patel <hemitvpatel@gmail.com>
2025-09-14 22:16:05 -0400

    implemented ai functions into the route backend

--- app/api/chat/route.ts
+++ app/api/chat/route.ts
@@ -1,5 +1,14 @@
-export const POST = () => {
+import { generateResponses, searchEmbeddings } from "@/lib/ai";
+import { NextRequest } from "next/server"
+
+export const POST = async (request: NextRequest) => {
+  const { userQuery, country } = await request.json();
+  const embeddings = await searchEmbeddings(userQuery);
+  const contexts = embeddings.map(embedding => embedding.payload.text) as string[];
+
+  const response = await generateResponses(userQuery, country, contexts);
+  
   return Response.json({
-    message: "Hello World!"
-  })
+    response
+  });
 }
