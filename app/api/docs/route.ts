addb2f003ad35e1a569d14cee031b7e244d3cdce
Hemit Patel <hemitvpatel@gmail.com>
2025-09-15 22:17:11 -0400

    added the document ai completion stuff

--- app/api/docs/route.ts
+++ app/api/docs/route.ts
@@ -2,7 +2,14 @@ import { generateResponses, searchEmbeddings } from "@/lib/ai";
 import { NextRequest } from "next/server"
 
 export const POST = async (request: NextRequest) => {
+  const { userQuery, country } = await request.json();
+  const embeddings = await searchEmbeddings(userQuery, "legislative_documents");
+  const contexts = embeddings.map(embedding => embedding.payload.text) as string[];
+
+  // DO SOME COOL UNIQUE STUFF HERE WHICH WILL HELP USERS WITH UNDERSTANDING LEGISLATIVE DOCUMENTS
+  const response = await generateResponses(userQuery, country, contexts);
+
   return Response.json({
-    health: "ok"
-  })
+    response
+  });
 }
