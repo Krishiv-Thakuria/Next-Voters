be80748fe72321406957fbe5d5f5c72ef05beb9d
Hemit Patel <hemitvpatel@gmail.com>
2025-09-20 21:04:10 -0400

    added some info about supported country

---
+++ data/supported-country.ts
@@ -0,0 +1,21 @@
+const supportedCountries = [
+  { 
+    code: 'US', 
+    name: 'United States',
+    politicalParties: [
+        { name: 'Democratic Party'},
+        { name: 'Republican Party'}
+    ] 
+},
+  { 
+    code: 'CA', 
+    name: 'Canada',
+    politicalParties: [
+        { name: 'Liberal Party'},
+        { name: 'Conservative Party'},
+        { name: 'New Democratic Party (NDP)'},
+    ]
+}
+]
+
+export default supportedCountries;
