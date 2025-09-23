b46b60a2773f52e1c7e4b46694f49ed668443fe0
Hemit Patel <hemitvpatel@gmail.com>
2025-09-23 11:17:49 -0400

    add some stuff

--- lib/preferences.ts
+++ lib/preferences.ts
@@ -1,3 +1,20 @@
-export const handleSetPreference = () => {
-    
+export const handleSetPreference = (
+  election: string,
+  region: string,
+  party: string,
+) => {
+  window.localStorage.setItem(
+    'preference',
+    JSON.stringify({
+      election,
+      region,
+      party,
+    })
+  )
+}
+
+export const handleGetPreference = () => {
+    const preference = window.localStorage.getItem('preference')
+    const preferenceJSON = preference ? JSON.parse(preference) : null
+    return preferenceJSON
 }
