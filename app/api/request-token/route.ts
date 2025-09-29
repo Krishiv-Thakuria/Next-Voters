47ae29940e8c0232f5f56133eceb87dfaa57f280
Hemit Patel <hemitvpatel@gmail.com>
2025-09-29 10:50:21 -0400

    ensure the user is an admin by cross checking with db

--- app/api/request-token/route.ts
+++ app/api/request-token/route.ts
@@ -1,18 +1,25 @@
 import { NextRequest, NextResponse } from "next/server";
 import { handleCreateToken } from "@/lib/jwt"; 
 import { transporter } from "@/lib/nodemailer";
+import { handleGetUser } from "@/lib/admin-user";
 
 export const POST = async (req: NextRequest) => {
   try {
     const { email } = await req.json();
     if (!email) {
-      return NextResponse.json({ error: "Email is required" }, { status: 400 });
+      throw new Error("Email has not been provided.")
+    }
+
+    const isUserAdmin = handleGetUser(email)
+
+    if (!isUserAdmin) {
+        throw new Error("User is not an admin.")
     }
 
     const token = handleCreateToken(email);
 
     await transporter.sendMail({
-      from: `"My App" <${process.env.GMAIL_USER}>`,
+      from: `Next Voter's Internal Platform`,
       to: email,
       subject: "Your access token (only valid for 5 minutes)",
       text: `Use the following token: ${token}`,
@@ -20,7 +27,6 @@ export const POST = async (req: NextRequest) => {
 
     return NextResponse.json({ success: true, message: "Email sent" });
   } catch (error: any) {
-    console.error("Email error:", error);
     return NextResponse.json({ error: "Failed to send email" }, { status: 500 });
   }
 };
--- lib/admin-user.ts
+++ lib/admin-user.ts
@@ -1,9 +1,10 @@
 import { db } from "./database"
 
-export const handleGetUser = async () => {
+export const handleGetUser = async (email: string) => {
     const row = await db
         .selectFrom("admin_table")
         .selectAll()
+        .where('email', '=', email)
         .executeTakeFirst() 
 
     return row ?? 0
