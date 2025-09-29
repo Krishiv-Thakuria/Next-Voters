c8ffa7425a04d489ef67ea0b4e068f7d84b9b3da
Hemit Patel <hemitvpatel@gmail.com>
2025-09-29 10:57:13 -0400

    remove unneeded comments

--- lib/nodemailer.ts
+++ lib/nodemailer.ts
@@ -3,7 +3,7 @@ import nodemailer from "nodemailer"
 export const transporter = nodemailer.createTransport({
       service: "gmail",
       auth: {
-        user: process.env.GMAIL_USER, // your Gmail address
-        pass: process.env.GMAIL_PASS, // your App Password (not your normal Gmail password)
+        user: process.env.GMAIL_USER,
+        pass: process.env.GMAIL_PASS, 
       },
 });
