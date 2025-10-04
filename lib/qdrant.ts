fe1a45017051b280055b8826b8dd96c7fa14ea61
Hemit Patel <hemitvpatel@gmail.com>
2025-10-03 22:34:07 -0400

    remove warning from qdrant

--- lib/qdrant.ts
+++ lib/qdrant.ts
@@ -2,5 +2,6 @@ import {QdrantClient} from '@qdrant/qdrant-js';
 
 export const client = new QdrantClient({ 
     url: process.env.QDRANT_URL,
-    apiKey: process.env.QDRANT_API_KEY
+    apiKey: process.env.QDRANT_API_KEY,
+    checkCompatibility: false
 })
