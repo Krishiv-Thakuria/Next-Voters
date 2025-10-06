ef150a45a2d43f617bead0688c7eefc1b3e25aca
Hemit Patel <hemitvpatel@gmail.com>
2025-10-06 11:56:47 -0400

    remove the filtering based off of election type

--- app/api/chat/route.ts
+++ app/api/chat/route.ts
@@ -1,17 +1,17 @@
 import { generateResponses, searchEmbeddings } from "@/lib/ai";
 import { NextRequest } from "next/server"
 
-// This includes the list of supported countries and their political parties
+// This includes the list of supported countries and their political parties (switch to db later on)
 import supportedCountriesDetails from "@/data/supported-regions";
 
 import { SupportedCountry } from "@/types/supported-regions";
 
 export const POST = async (request: NextRequest) => {
-  const { query, region, electionType } = await request.json();
+  const { query, region,  } = await request.json();
   const responses = [];
 
   const regionDetail = supportedCountriesDetails.find(
-    regionItem => regionItem.name === region && regionItem.elections.includes(electionType)
+    regionItem => regionItem.name === region 
   );
 
   if (!regionDetail) {
