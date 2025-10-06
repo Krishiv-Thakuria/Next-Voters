4b86aa942677320204345c839229985b9b234267
Hemit Patel <hemitvpatel@gmail.com>
2025-10-06 11:53:14 -0400

    do not hide component when pathname is /chat

--- components/root.tsx
+++ components/root.tsx
@@ -3,14 +3,12 @@
 import React, { useState, FC, ReactNode } from 'react'
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import Header from './common/header';
-import { usePathname } from 'next/navigation';
 
 interface RootInterface {
     children: ReactNode
 }
 
 const Root: FC<RootInterface> = ({ children }) => {
-    const pathname = usePathname();
     const [queryClient] = useState(
     () =>
       new QueryClient({
@@ -25,9 +23,7 @@ const Root: FC<RootInterface> = ({ children }) => {
   
   return (
     <QueryClientProvider client={queryClient}>
-        {pathname !== '/chat' &&
-          <Header />
-        }
+        <Header />
         {children}
     </QueryClientProvider>
   )
