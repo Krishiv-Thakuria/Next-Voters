5d0da8807a9871da1ce0edce6e4800013312501e
Hemit Patel <hemitvpatel@gmail.com>
2025-10-07 10:53:21 -0400

    remove unneeded comments + make expiration for token 2m

--- server-actions/jwt.ts
+++ server-actions/jwt.ts
@@ -2,7 +2,6 @@
 
 import jwt from "jsonwebtoken";
 
-// returns a string token
 export const handleCreateToken = async (email: string): Promise<string> => {
   if (!process.env.JWT_PRIVATE_KEY) {
     throw new Error("JWT_PRIVATE_KEY is not defined in environment");
@@ -11,11 +10,11 @@ export const handleCreateToken = async (email: string): Promise<string> => {
   const payload = {
     allowed: true,
     email,
-    iat: Math.floor(Date.now() / 1000), // issued at
+    iat: Math.floor(Date.now() / 1000), 
   };
 
   const token = jwt.sign(payload, process.env.JWT_PRIVATE_KEY, {
-    expiresIn: "5m", // set expiration for safety
+    expiresIn: "2m", 
   });
 
   return token;
