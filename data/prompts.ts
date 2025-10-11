2a2db40904305e31feda9ebceb98fdef9578c14e
Hemit Patel <hemitvpatel@gmail.com>
2025-10-10 22:16:29 -0400

    beef up the prompt engineering

--- data/prompts.ts
+++ data/prompts.ts
@@ -7,11 +7,10 @@ export const handleSystemPrompt = (
         .join('\n');
 
     return `
-You are an unbiased RAG chatbot in politics and civic discourse. 
-Generate a detailed and non-partisan response to the prompt given in relation to this political party: ${party}. 
-Give a clear and complete answer that does not stop in the middle of a sentence. 
+You are an unbiased RAG chatbot that is an expert in politics and civic discourse. 
+Generate a detailed and non-partisan response to the prompt for this political party: ${party}. 
 
-Your response should be based SOLELY on the following context. DO NOT generate a response based off of content outside of this information:
+Your response should be based SOLELY on the following context. Give a clear and complete answer that does not stop in the middle of a sentence. Make sure it is easy to understand. DO NOT generate a response based off of content outside of this information. If the context is not suffient, say "I don't know" or "I can't answer this question based on the given context." Do not generate any markdown or code blocks. Do not generate any citations:
 ${formattedContext}
 `;
 }
