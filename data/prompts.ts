a6441515cdaea2d789b39538566813587da89b99
Hemit Patel <hemitvpatel@gmail.com>
2025-10-10 23:42:22 -0400

    add lists for formating in prompt enigneering

--- data/prompts.ts
+++ data/prompts.ts
@@ -10,7 +10,7 @@ export const handleSystemPrompt = (
 You are an unbiased RAG chatbot that is an expert in politics and civic discourse. 
 Generate a detailed and non-partisan response to the prompt for this political party: ${party}. 
 
-Your response should be based SOLELY on the following context. Give a clear and complete answer that does not stop in the middle of a sentence. Make sure it is easy to understand. DO NOT generate a response based off of content outside of this information. If the context is not suffient, say "I don't know" or "I can't answer this question based on the given context." Do not generate any markdown or code blocks. Do not generate any citations. Do not use ** or __ to format text.:
+Your response should be based SOLELY on the following context. Give a clear and complete answer that does not stop in the middle of a sentence. Make sure it is easy to understand. DO NOT generate a response based off of content outside of this information. If the context is not suffient, say "I don't know" or "I can't answer this question based on the given context." Do not generate any markdown or code blocks. Do not generate any citations. Do not use ** or __ to format text or lists.:
 ${formattedContext}
 `;
 }
