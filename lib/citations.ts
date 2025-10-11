fb9696d3b75ca7fb3cdf67be98480a6f2d19e90f
Hemit Patel <hemitvpatel@gmail.com>
2025-10-10 21:33:20 -0400

    remove params + rename the iterations variable to citations

--- lib/citations.ts
+++ lib/citations.ts
@@ -1,8 +1,8 @@
 import { Citation } from "@/types/citations";
 
 export const removeDuplicateCitations = (citations: Citation[]) => {
-    const uniqueCitations = citations.filter((citation, index) => {
-        return citations.findIndex(c => c.document_name === citation.document_name) === index;
+    const uniqueCitations = citations.filter((_, index) => {
+        return citations.findIndex((citation) => citation.document_name === citation.document_name) === index;
     });
     return uniqueCitations;
 }
