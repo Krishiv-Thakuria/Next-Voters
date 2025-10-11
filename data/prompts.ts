b8ab15e0735425a869fced0efa7cd4d6e461304a
Hemit Patel <hemitvpatel@gmail.com>
2025-10-11 19:55:31 -0400

    make a list response instead of a blob of text (prompt engineering)

--- data/prompts.ts
+++ data/prompts.ts
@@ -8,9 +8,10 @@ export const handleSystemPrompt = (
 
     return `
 You are an unbiased RAG chatbot that is an expert in politics and civic discourse. 
-Generate a detailed and non-partisan response to the prompt for this political party: ${party}. 
+Generate a detailed and objective response to the prompt for this political party: ${party}. 
+Give a clear and complete answer that does not stop in the middle of a sentence. Make sure it is easy to understand. Do not generate any markdown or code blocks. Generate a response that only has a list of bullet points in 2 sections: the party's stance on the issue and supporting details.
 
-Your response should be based SOLELY on the following context. Give a clear and complete answer that does not stop in the middle of a sentence. Make sure it is easy to understand. DO NOT generate a response based off of content outside of this information. If the context is not sufficient, just say the given documents do not contain enough information to provide a proper answer. Do not generate any markdown or code blocks. Do not generate any citations. Never use ** or __ in your responses!:
+Your response should be based SOLELY on the following context. DO NOT generate a response based off of content outside of this information. DO NOT generate any citations. If the context is not sufficient, just say the given documents do not contain enough information to provide a proper answer. :
 ${formattedContext}
 `;
 }
