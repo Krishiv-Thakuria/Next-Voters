1faa8a5d766c13761db0351d3cc17560acc36859
Hemit Patel <hemitvpatel@gmail.com>
2025-10-13 22:52:45 -0400

    integrated the basic setup for clerk

--- components/common/header.tsx
+++ components/common/header.tsx
@@ -1,28 +1,36 @@
-"use client"
+"use client";
 
-import React from 'react'
-import { usePathname } from 'next/navigation'
+import React from "react";
+import { usePathname } from "next/navigation";
+import { SignOutButton, UserButton } from "@clerk/nextjs";
 
 const Header = () => {
   const pathname = usePathname();
+
   return (
-      <header className="w-full bg-white">
-        <div className="max-w-[1200px] mx-auto px-6 py-4 flex justify-between items-center">
-
-          <div className="flex items-center gap-4">
-            <a
-              href={pathname === "/fellowship" ? "/" :"/fellowship"}
-              className="bg-[#E12D39] text-[12px] text-white px-4 py-2 rounded font-medium font-poppins"
-            >
-              {pathname === "/fellowship" ? "HOME" : "BECOME A FELLOW"}
-            </a>
-        </div>
+    <header className="w-full bg-white">
+      <div className="max-w-[1200px] mx-auto px-6 py-4 flex justify-between items-center">
+        <div className="flex items-center gap-4">
           <div className="flex items-center">
-            <span className="text-[18px] font-bold text-gray-900 font-poppins">NV</span>
+            <span className="text-[18px] font-bold text-gray-900 font-poppins mr-4">
+              NV
+            </span>
           </div>
+          <a
+            href={pathname === "/fellowship" ? "/" : "/fellowship"}
+            className="bg-[#E12D39] text-[12px] text-white px-4 py-2 rounded font-medium font-poppins"
+          >
+            {pathname === "/fellowship" ? "HOME" : "BECOME A FELLOW"}
+          </a>
         </div>
-      </header>  
-    )
-}
 
-export default Header
+        <div className="flex items-center gap-2">
+          <UserButton />
+          <SignOutButton />
+        </div>
+      </div>
+    </header>
+  );
+};
+
+export default Header;
