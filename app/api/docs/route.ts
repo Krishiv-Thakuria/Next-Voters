d07ef5aecd3e6b334ce614284ea659ab83ff0303
Hemit Patel <hemitvpatel@gmail.com>
2025-10-16 07:47:25 -0400

    use nextresponse instead of just response so we can use json() static method on class

--- app/api/docs/route.ts
+++ app/api/docs/route.ts
@@ -1,10 +1,10 @@
-import { NextRequest } from "next/server"
+import { NextRequest, NextResponse } from "next/server"
 
 /*
 export const POST = async (request: NextRequest) => {
   const { userQuery, country } = await request.json();
   const embeddings = await searchEmbeddings(userQuery, "legislative_documents");
-
+  
   const contexts = [];
   const citations = [];
 
@@ -16,7 +16,7 @@ export const POST = async (request: NextRequest) => {
   // DO SOME COOL UNIQUE STUFF HERE WHICH WILL HELP USERS WITH UNDERSTANDING LEGISLATIVE DOCUMENTS
   const response = await generateResponses(userQuery, country, contexts);
 
-  return Response.json({
+  return NextResponse.json({
     response,
     citations
   });
@@ -24,7 +24,7 @@ export const POST = async (request: NextRequest) => {
 */
 
 export const GET = async (request: NextRequest) => {
-  return Response.json({
+  return NextResponse.json({
     message: "Coming soon!"
   })
 }
