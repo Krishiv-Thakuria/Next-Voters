946bb3cc89669f86eeef3ad9fba8bbbceddcc8e0
Hemit Patel <hemitvpatel@gmail.com>
2025-10-16 07:59:18 -0400

    remove unneeded wrapper for clerk ui stuff

--- components/common/header.tsx
+++ components/common/header.tsx
@@ -1,36 +1,54 @@
 "use client";
 
 import React, { FC } from "react";
-import { usePathname } from "next/navigation";
-import ClerkUIWrapper from "@/wrapper/clerk/clerk-ui-wrapper";
 import headerItems from "@/data/header";
+import { 
+  SignedIn, 
+  SignedOut, 
+  UserButton, 
+  SignOutButton, 
+  SignInButton 
+} from "@clerk/nextjs";
 
 const Header: FC = () => {
-  const pathname = usePathname();
-
   return (
     <header className="w-full bg-white">
-      <div className="max-w-[1200px] mx-auto px-6 py-4 flex justify-between items-center">
+      <div className="max-w-[1200px] mx-auto px-6 py-4 flex flex-col md:flex-row justify-between items-center">
         <div className="flex items-center gap-4">
           <div className="flex items-center">
             <span className="text-[18px] font-bold text-gray-900 font-poppins mr-4">
               NV
             </span>
           </div>
-          {headerItems.map((item) => (
-            <a
-              key={item.name}
-              href={item.href}
-              className="bg-[#E12D39] text-[12px] text-white px-4 py-2 rounded font-medium font-poppins"
-            >
-              {item.name}
-            </a>
-          ))}
+          <a
+            href={"/fellowship"}
+            className="bg-[#E12D39] text-[12px] text-white px-4 py-2 rounded font-medium font-poppins"
+          >
+            {"BECOME A FELLOW"}
+          </a>
+
+          <div className="flex flex-col md:flex-row gap-2">
+            {headerItems.map((item) => (
+              <a
+                key={item.name}
+                href={item.href}
+                className="text-md text-gray-900 px-4 py-2 rounded font-medium font-poppins"
+              >
+                {item.name}
+              </a>
+            ))}
+          </div>
         </div>
 
         <div className="flex items-center gap-2">
-          <ClerkUIWrapper />
-        </div>
+        <SignedIn>
+          <UserButton />
+          <SignOutButton />
+        </SignedIn>
+        <SignedOut>
+          <SignInButton />
+        </SignedOut>
+      </div>
       </div>
     </header>
   );
--- components/common/root.tsx
+++ components/common/root.tsx
@@ -4,36 +4,7 @@ import { useState } from 'react'
 import type { FC, ReactNode } from 'react'
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import React from "react";
-import { usePathname } from "next/navigation";
-import ClerkUIWrapper from "@/wrapper/clerk/clerk-ui-wrapper";
-
-const Header = () => {
-  const pathname = usePathname();
-
-  return (
-    <header className="w-full bg-white">
-      <div className="max-w-[1200px] mx-auto px-6 py-4 flex justify-between items-center">
-        <div className="flex items-center gap-4">
-          <div className="flex items-center">
-            <span className="text-[18px] font-bold text-gray-900 font-poppins mr-4">
-              NV
-            </span>
-          </div>
-          <a
-            href={pathname === "/fellowship" ? "/" : "/fellowship"}
-            className="bg-[#E12D39] text-[12px] text-white px-4 py-2 rounded font-medium font-poppins"
-          >
-            {pathname === "/fellowship" ? "HOME" : "BECOME A FELLOW"}
-          </a>
-        </div>
-
-        <div className="flex items-center gap-2">
-          <ClerkUIWrapper />
-        </div>
-      </div>
-    </header>
-  );
-};
+import Header from "@/components/common/header"
 
 interface RootProps {
   children: ReactNode;
