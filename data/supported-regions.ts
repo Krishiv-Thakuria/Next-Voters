abba210caee9141f916510e5d74149be80f758b3
Hemit Patel <hemitvpatel@gmail.com>
2025-10-26 11:11:47 -0400

    remove states and provinces for supported regions

--- data/supported-regions.ts
+++ data/supported-regions.ts
@@ -22,47 +22,6 @@ export const supportedRegionDetails: SupportedRegionDetails[] = [
       "Republican Party"
     ],
     collectionName: "collection-us"
-  },
-
-  // U.S. States
-  {
-    code: "CA-US",
-    name: "California",
-    politicalParties: [
-      "California Democractic Party", 
-      "California Republican Party"
-    ],
-    collectionName: "collection-us-california"  
-  },
-  {
-    code: "TX-US",
-    name: "Texas",
-    politicalParties: [
-      "Texas Democratic Party", 
-      "Texas Republican Party"
-    ],
-    collectionName: "collection-us-texas"
-  },
-
-  // Canadian Provinces
-  {
-    code: "ON-CA",
-    name: "Ontario",
-    politicalParties: [
-      "Liberal Party of Ontario",
-      "Progressive Conservative Party of Ontario",
-      "Ontario New Democratic Party (NDP)",
-    ],
-    collectionName: "collection-ca-ontario"
-  },
-  {
-    code: "QC-CA",
-    name: "Quebec",
-    politicalParties: [
-      "Quebec Liberal Party",
-      "Parti Québécois",
-    ],
-    collectionName: "collection-ca-quebec"
   }
 ]
 
