e5bd0be9f08b0334afd2df0117bd20cf2fdee5d5
Hemit Patel <hemitvpatel@gmail.com>
2025-10-26 21:14:08 -0400

    make proper responsive header

--- components/common/header.tsx
+++ components/common/header.tsx
@@ -7,15 +7,17 @@ import { LogoutLink } from "@kinde-oss/kinde-auth-nextjs/components";
 const Header: FC = () => {
   const [isOpen, setIsOpen] = useState(false);
 
+  const toggleMenu = () => {
+    setIsOpen((prevState) => !prevState);
+  };
+
   return (
     <header className="w-full bg-white">
       <div className="max-w-[1200px] mx-auto px-6 py-4 flex flex-col md:flex-row justify-between items-center">
         <div className="flex items-center gap-4">
-          <div className="flex items-center">
-            <span className="text-[18px] font-bold text-gray-900 font-poppins mr-4">
-              NV
-            </span>
-          </div>
+          <span className="text-[18px] font-bold text-gray-900 font-poppins">
+            NV
+          </span>
           <a
             href={"/fellowship"}
             className="bg-[#E12D39] text-[12px] text-white px-4 py-2 rounded font-medium font-poppins"
@@ -24,9 +26,9 @@ const Header: FC = () => {
           </a>
 
           <button
+            onClick={toggleMenu}
+            aria-label={`${isOpen ? "Close" : "Open"} menu`}
             className="md:hidden"
-            onClick={() => setIsOpen(!isOpen)}
-            aria-label="Open menu"
           >
             <svg
               className="w-6 h-6 fill-current"
@@ -35,26 +37,24 @@ const Header: FC = () => {
             >
               <path
                 d={`${
-                  isOpen ? "M18 6L6 18" : "M3 6H21V8H3V6M3 12H21V14H3V12M3 18H21V20H3V18Z"
+                  isOpen ? "M6 18L18 6M6 6l12 12" : "M3 6H21V8H3V6M3 12H21V14H3V12M3 18H21V20H3V18Z"
                 }`}
               />
             </svg>
           </button>
         </div>
 
-        {isOpen && (
-          <div className="flex flex-col gap-2">
-            {headerItems.map((item) => (
-              <a
-                key={item.name}
-                href={item.href}
-                className="text-md text-gray-900 px-4 py-2 rounded font-medium font-poppins block"
-              >
-                {item.name}
-              </a>
-            ))}
-          </div>
-        )}
+        <nav className={`md:flex ${isOpen ? "block" : "hidden"}`}>
+          {headerItems.map((item) => (
+            <a
+              key={item.name}
+              href={item.href}
+              className="text-md text-gray-900 px-4 py-2 rounded font-medium font-poppins block"
+            >
+              {item.name}
+            </a>
+          ))}
+        </nav>
 
         <LogoutLink className="md:block hidden px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 rounded">
           Sign Out
