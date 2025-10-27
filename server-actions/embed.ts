0c40d276350108fe5b2ecfba30d597f41eef6e4d
Hemit Patel <hemitvpatel@gmail.com>
2025-10-26 20:27:25 -0400

    remove console error

--- server-actions/embed.ts
+++ server-actions/embed.ts
@@ -85,7 +85,6 @@ export async function embedPdfAction(formData: {
       message: "Embeddings added successfully!" 
     };
   } catch (error: any) {
-    console.error(error)
     return { 
       success: false, 
       error: error.message || "Internal Server Error" 
