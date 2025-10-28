86a8d4ba4d11edd7f65611a3bdfe4e5927f61829
Hemit Patel <hemitvpatel@gmail.com>
2025-10-28 15:09:24 -0400

    update data with next voter personallel

--- data/team-members.ts
+++ data/team-members.ts
@@ -1,26 +1,26 @@
 export const teamMembers = [
     {
-      name: 'Bonnie Green',
-      role: 'CEO & Web Developer',
+      name: 'Krishiv Thakuria',
+      role: 'CEO',
       image: '/profile-pics/placeholder.png',
-      description: 'Bonnie drives the technical strategy of the flowbite platform and brand.'
+      description: 'Krishiv Thakuria is the CEO of NextVoters. He is a tech visioanary leading the company with his innovative ideas and vision.'
     },
     {
-      name: 'Jese Leos',
+      name: 'Hemit Patel',
       role: 'CTO',
       image: '/profile-pics/placeholder.png',
-      description: 'Jese drives the technical strategy of the flowbite platform and brand.'
+      description: 'Hemit Patel is the CTO of NextVoters. He is a software engineer with a passion for building innovative and user-friendly applications.'
     },
     {
-      name: 'Michael Gough',
-      role: 'Senior Front-end Developer',
+      name: 'Danielle Dees',
+      role: 'COO',
       image: '/profile-pics/placeholder.png',
-      description: 'Michael drives the technical strategy of the flowbite platform and brand.'
+      description: 'Danielle Dees is the COO of NextVoters. She is a tech leader with a passion for building innovative and user-friendly applications.'
     },
     {
-      name: 'Lana Byrd',
-      role: 'Marketing & Sale',
+      name: 'Kofi',
+      role: 'Advisor',
       image: '/profile-pics/placeholder.png',
-      description: 'Lana drives the technical strategy of the flowbite platform and brand.'
+      description: 'Kofi is the Advisor of NextVoters. He is a tech leader with a passion for building innovative and user-friendly applications.'
     }
   ];
