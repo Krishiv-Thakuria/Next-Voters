9f56eb02eede094bfe4b8ec8e506cfbe6c5fb5c7
Hemit Patel <hemitvpatel@gmail.com>
2025-12-15 18:11:12 -0500

    update verison so i can upload the readme doc

--- civic-line-cli/setup.py
+++ civic-line-cli/setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="civic_line_cli",
-    version="0.6",  
+    version="0.7",  
     packages=find_packages(),
     install_requires=[
         "openai",
